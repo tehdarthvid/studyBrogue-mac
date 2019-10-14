346f04288e6b0010f90a49d11808533bcc204c96
tehdarthvid <darthvid@hotmail.com>
2019-10-14 16:24:12 +0800

    Auto-generated from creating a new game project in Xcode 11.1.

---
+++ PlatformCode/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  Brogue
+//
+//  Created by Raymund Vidar on 10/11/19.
+//  Copyright © 2019 darthvid. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    // Darth: For new AppActive detection.
+    
+    func applicationDidBecomeActive(_ aNotification: Notification) {
+        // Sent by the default notification center immediately after the application becomes active.
+        
+        //print("\(#function)")
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "AppActive"), object: self)
+    }
+    
+    func applicationWillResignActive(_ aNotification: Notification) {
+        // Sent by the default notification center immediately before the application is deactivated.
+        
+        //print("\(#function)")
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "AppInactive"), object: self)
+    }
+}
